3ddd0695a04cd23277edb705f9a0fa09a8e6c514
Aman Sharma <aman.sharma@greyspacedigital.com>
2025-04-23 02:45:54 +0530

    Build : Add Me section structure

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 import Image from "next/image";
 import { NavItems, NavDropDownItems } from "./NavItems";
 import ThemeSwitch from "./ThemeSwitch";
-import { Squirrel } from "lucide-react"
-
+import { Squirrel } from "lucide-react";
+import { pageBg } from "../../../utils/styles";
 
 const NavBar = () => {
   return (
-    <nav className="mx-auto flex max-w-[900px] items-center justify-between gap-3 px-4 pt-4">
+    <nav
+      className={`${pageBg} mx-auto flex max-w-[900px] items-center justify-between gap-3 px-4 pt-4 opacity-95`}
+    >
       <Link href="/">
-      <Squirrel size={35}/>
+        <Squirrel size={35} />
       </Link>
       <ul className="gap-x-12 hidden text-sm md:flex cursor-pointer">
         <NavItems />
